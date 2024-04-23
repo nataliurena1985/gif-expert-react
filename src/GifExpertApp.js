b424b73e2ccc6b3bb4c8725d35ea826a8896e069
claudia <claudianataliurena@gmail.com>
2024-04-23 11:52:08 -0300

    mapeo con map una lista

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,9 +1,18 @@
 import React from "react";
 
 export const GifExpertApp = () => {
+  const categories = ["OnePunch", "Samuray", "Dragon Ball"];
   return (
     <>
       <h2>GifExpertApp </h2>
+      <hr />
+      <button>Agregar</button>
+
+      <ol>
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
+      </ol>
     </>
   );
 };
