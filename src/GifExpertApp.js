a7dcbea4242dde54c5ed4d63a2e08fdb7293d98c
claudia <claudianataliurena@gmail.com>
2024-04-23 12:31:32 -0300

    agregando un elemento a la lista con spread

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const GifExpertApp = () => {
-  const categories = ["OnePunch", "Samuray", "Dragon Ball"];
+  //const categories = ["OnePunch", "Samuray", "Dragon Ball"];
+
+  const [categories, setCategories] = useState([
+    "OnePunch",
+    "Samuray",
+    "Dragon Ball",
+  ]);
+
+  const handleAdd = () => {
+    // categories.push("hunterX");
+    setCategories([...categories, "hunterX"]);
+  };
+
   return (
     <>
       <h2>GifExpertApp </h2>
       <hr />
-      <button>Agregar</button>
+      <button onClick={handleAdd}>Agregar</button>
 
       <ol>
         {categories.map((category) => {
