ee68f3b9665a48800340438fe8b6da0f7d49b3f9
claudia <claudianataliurena@gmail.com>
2024-04-24 19:55:26 -0300

    helpers getGifs

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,30 +1,32 @@
 import React, { useState, useEffect } from "react";
 import { GifGridItem } from "./GifGridItem";
+import { GetGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
   const [images, setImages] = useState([]);
 
   useEffect(() => {
-    GetGifs();
-  }, []);
+    GetGifs(category).then(setImages);
+  }, [category]);
 
-  const GetGifs = async () => {
-    const url =
-      "https://api.giphy.com/v1/gifs/search?q=Rick+and+Morty&limit=10&api_key=v5oiBlkhikozLm82nQrRe1dW55nFKCSN";
+  // const GetGifs = async () => {
+  //    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(
+  //      category
+  //    )}&limit=10&api_key=v5oiBlkhikozLm82nQrRe1dW55nFKCSN`;
 
-    const resp = await fetch(url);
-    const { data } = await resp.json();
+  //   const resp = await fetch(url);
+  //   const { data } = await resp.json();
 
-    const gifs = data.map((img) => {
-      return {
-        id: img.id,
-        title: img.title,
-        url: img.images?.downsized_medium.url,
-      };
-    });
-    console.log(gifs);
-    setImages(gifs);
-  };
+  //   const gifs = data.map((img) => {
+  //     return {
+  //       id: img.id,
+  //       title: img.title,
+  //       url: img.images?.downsized_medium.url,
+  //     };
+  //   });
+  //   console.log(gifs);
+  //   setImages(gifs);
+  // };
 
   return (
     <>
